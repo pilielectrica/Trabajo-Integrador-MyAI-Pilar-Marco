a6a9f7df5a72100cdeb5007815700f04c5acbe6a
pilielectrica <pilarmarco.musica@gmail.com>
2023-11-30 17:34:15 -0300

    clases creadas, main maqueta creado

--- Trabajo Integrador MyAI Pilar Marco/Trabajo Integrador MyAI Pilar Marco.cpp
+++ Trabajo Integrador MyAI Pilar Marco/Trabajo Integrador MyAI Pilar Marco.cpp
@@ -1,19 +1,90 @@
-// Trabajo Integrador MyAI Pilar Marco.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
+//#include "crosshair.h"
+//#include "enemigos.h"
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 #include <iostream>
+using namespace sf;
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+int main() {
+	srand(time(NULL));
+	Event evt;
+	//Crosshair _crosshair;
+	//Enemigos _enemigo;
+	//_crosshair.seteartexturacrosshair();
+	//_enemigo.setearenemigo();
+	int score = 0;
+	bool disparo = false;
+	Text mensajepuntos;
+	Font font;
+	mensajepuntos.setFont(font);
+	mensajepuntos.setCharacterSize(24);
+	mensajepuntos.setPosition(10, 10);
+	mensajepuntos.setFillColor(sf::Color::White);
+	font.loadFromFile("SIXTY.TTF");
+	bool ganaste = false;
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+	sf::RenderWindow App(sf::VideoMode(600, 600, 32),
+		"Que ventana horrible");
+	App.setMouseCursorVisible(false);
+
+	// Loop principal
+	while (App.isOpen()) {
+		while (App.pollEvent(evt)) {
+			Vector2i cursorPosition = Mouse::getPosition(App);
+			/////Procesar eventos//////
+			switch (evt.type) {
+				/////Si se cerro la ventana//////
+			case sf::Event::Closed:
+				App.close();
+				break;
+			case sf::Event::MouseMoved:
+				_crosshair.actualizarPosicionCursor(evt.mouseMove.x, evt.mouseMove.y);
+				break;
+
+			}Vector2f crosshairPosition = _crosshair.mostrarsprite().getPosition();
+			if (_enemigo.aparecer().getGlobalBounds().contains(crosshairPosition.x, crosshairPosition.y))
+			{
+				if (evt.type == Event::MouseButtonReleased)
+				{
+
+					score += 1;
+					if (score >= 5)
+					{
+						ganaste = true;
+					}
+				}
+			}
+
+			_enemigo.moverse();
+		}
+
+		if (ganaste)
+		{
+
+			App.clear();
+			Text textoGanaste("GANASTE", font, 36);
+			textoGanaste.setFillColor(Color::White);
+			textoGanaste.setPosition(200, 200);
+			App.draw(textoGanaste);
+			App.display();
+		}
+		else
+		{
+			App.clear();
+
+			App.draw(_enemigo.aparecer());
+			App.draw(_crosshair.mostrarsprite());
+			mensajepuntos.setString("Puntaje: " + to_string(score));
+			App.draw(mensajepuntos);
+			// Mostramos la ventana
+			App.display();
+
+		}
+
+
+	}
+
+	return 0;
+
